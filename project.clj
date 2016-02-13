e0853b9042747bef0809c365722e67a2a65b658f
Paul L. Snyder <paul@pataprogramming.com>
2016-02-12 21:25:24 -0500

    Update dependencies to latest versions

--- project.clj
+++ project.clj
@@ -3,10 +3,10 @@
   :url "http://github.io/pataprogramming/loom-jung/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [seesaw "1.4.4-SNAPSHOT"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [seesaw "1.4.5"]
                  [net.sf.jung/jung-api "2.0.1"]
                  [net.sf.jung/jung-graph-impl "2.0.1"]
                  [net.sf.jung/jung-io "2.0.1"]
                  [net.sf.jung/jung-visualization "2.0.1"]
-                 [aysylu/loom "0.4.2"]])
+                 [aysylu/loom "0.5.4"]])
