aa31552ccfeddac7a4074cfa3b865029af7aa830
Paul L. Snyder <paul@pataprogramming.com>
2016-02-13 17:00:18 -0500

    Convert to use Seesaw's Configurable protocol

--- src/loom_jung/graph.clj
+++ src/loom_jung/graph.clj
@@ -1,4 +1,8 @@
 (ns loom-jung.graph
+  (:require [seesaw.config :refer [Configurable]]
+            [seesaw.options :refer [get-option-value apply-options option-map default-option option-provider]]
+            [seesaw.to-widget :refer [ToWidget]]
+            [seesaw.widget-options :only [widget-option-provider]])
   ;; (:require [taoensso.timbre :as timbre
   ;;            :refer (trace debug info warn error fatal spy with-log-level)])
   (:import (java.io StringWriter)
@@ -33,7 +37,7 @@
     (reify
       Predicate
       (evaluate [this o]
-        (f o))
+        (f (boolean o)))
       clojure.lang.IFn
       (invoke [this arg] (.evaluate this arg))
       (applyTo [this [arg]] (.evaluate this arg)))))
@@ -91,7 +95,6 @@
     (dosync
      (alter (:properties jgh) #(assoc-in % [v key] val)))))
 
-;;; FIXME: DRY!
 (defn jung-loc-step [old-val {:keys [val action func] :as val-action}]
   (case action
     :set
@@ -172,87 +175,42 @@
              (.setBackground Color/WHITE))]
     vv))
 
-(defn edge-renderer! [jvh er]
-  ;; er should be an instance of EdgeRenderer
-  (let [r (.getRenderer (:visualizer jvh))]
-    (.setEdgeRenderer r er)))
+(defn jung-visualizer [jgh]
+  (let [g  (:jg jgh)
+        l  (make-layout g 650 650 100 0.75)
+        vv (make-visualizer l 650 650)]
+    vv))
 
-(defn vertex-label-fn! [jvh f]
-  (let [rc (.getRenderContext (:visualizer jvh))]
-    (.setVertexLabelTransformer rc (make-transformer f))
-    jvh))
+(defn jung-static-visualizer [jgh vertex-xy-fn]
+  (let [g  (:jg jgh)
+        l  (make-static-layout g vertex-xy-fn 650 650)
+        vv (make-visualizer l 650 650)]
+    vv))
 
-(defn vertex-shape-fn! [jvh f]
-  (let [rc (.getRenderContext (:visualizer jvh))]
-    (.setVertexShapeTransformer rc (make-transformer f))
-    jvh))
+(extend-protocol Configurable
+  VisualizationViewer
+  (config* [target name] (get-option-value target name))
+  (config!* [target args] (apply-options target args)))
 
-(defn vertex-stroke-fn! [jvh f]
-  (let [rc (.getRenderContext (:visualizer jvh))]
-    (.setVertexStrokeTransformer rc (make-transformer f))
-    jvh))
+(defmacro r-set [method]
+  `(fn [vv# o#] (-> vv# .getRenderer (. ~method o#)))  )
 
-(defn vertex-draw-paint-fn! [jvh f]
-  (let [rc (.getRenderContext (:visualizer jvh))]
-    (.setVertexDrawPaintTransformer rc (make-transformer f))
-    jvh))
+(defmacro r-get [method]
+  `(fn [vv# ] (-> vv# .getRenderer (. ~method)))  )
 
-(defn vertex-fill-paint-fn! [jvh f]
-  (let [rc (.getRenderContext (:visualizer jvh))]
-    (.setVertexFillPaintTransformer rc (make-transformer f))
-    jvh))
+(defmacro rc-set-transform [method]
+  `(fn [vv# f#] (-> vv# .getRenderContext (. ~method (make-transformer f#)))))
 
-(defn vertex-font-fn! [jvh f]
-  (let [rc (.getRenderContext (:visualizer jvh))]
-    (.setVertexFontTransformer rc (make-transformer f))
-    jvh))
+(defmacro rc-set-predicate [method]
+  `(fn [vv# f#] (-> vv# .getRenderContext (. ~method (make-predicate f#)))))
 
-(defn edge-label-fn! [jvh f]
-  (let [rc (.getRenderContext (:visualizer jvh))]
-    (.setEdgeLabelTransformer rc (make-transformer f))
-    jvh))
+(defmacro rc-get [method]
+  `(fn [vv#] (-> vv# .getRenderContext (. ~method))))
 
-(defn edge-stroke-fn! [jvh f]
-  (let [rc (.getRenderContext (:visualizer jvh))]
-    (.setEdgeStrokeTransformer rc (make-transformer f))
-    jvh))
+(defmacro rc-get-predicate [method]
+  `(fn [vv#] (-> vv# .getRenderContext (. ~method) boolean)))
 
-(defn edge-draw-paint-fn! [jvh f]
-  (let [rc (.getRenderContext (:visualizer jvh))]
-    (.setEdgeDrawPaintTransformer rc (make-transformer f))
-    jvh))
-
-(defn arrow-draw-paint-fn! [jvh f]
-  (let [rc (.getRenderContext (:visualizer jvh))]
-    (.setArrowDrawPaintTransformer rc (make-transformer f))
-    jvh))
-
-(defn edge-draw-paint-fn! [jvh f]
-  (let [rc (.getRenderContext (:visualizer jvh))]
-    (.setEdgeDrawPaintTransformer rc (make-transformer f))
-    jvh))
-
-(defn edge-shape-fn! [jvh f]
-  (let [rc (.getRenderContext (:visualizer jvh))]
-    (.setEdgeShapeTransformer rc (make-transformer f))
-    jvh))
-
-(defn arrow-fill-paint-fn! [jvh f]
-  (let [rc (.getRenderContext (:visualizer jvh))]
-    (.setArrowFillPaintTransformer rc (make-transformer f))
-    jvh))
-
-(defn arrow-stroke-fn! [jvh f]
-  (let [rc (.getRenderContext (:visualizer jvh))]
-    (.setEdgeArrowStrokeTransformer rc (make-transformer f))
-    jvh))
-
-(defn edge-arrow-p! [jvh f]
-  (let [rc (.getRenderContext (:visualizer jvh))]
-    (.setEdgeArrowPredicate rc (make-predicate f))
-    jvh))
-
-(defn vertex-renderer! [jvh f]
+(defn vertex-renderer! [vv f]
   (let [vr (reify Renderer$Vertex
              (paintVertex [this render-context layout vertex]
                (let [graphics-context (.getGraphicsContext render-context)
@@ -265,21 +223,74 @@
                      y                (- (.getY center) (/ h 2))]
                  (.draw graphics-context component
                         (.getRendererPane render-context)
-                        x y w h true))))
-        vv (:visualizer jvh)]
+                        x y w h true))))]
     (-> vv (.getRenderer) (.setVertexRenderer vr))))
 
+(def jung-visualizer-options
+  (option-map
+   (default-option :vertex-label
+                   (rc-set-transform setVertexLabelTransformer)
+                   (rc-get getVertexLabelTransformer)
+                   "fn mapping from vertex to string to be used as a text label")
+   (default-option :vertex-draw
+                   (rc-set-transform setVertexDrawPaintTransformer)
+                   (rc-get getVertexDrawPaintTransformer)
+                   "fn mapping from vertex to Paint for drawing (e.g., outline color)")
+   (default-option :vertex-fill
+                   (rc-set-transform setVertexFillPaintTransformer)
+                   (rc-get getVertexFillPaintTransformer)
+                   "fn mapping from vertex to Paint for filling (inside the outline)")
+   (default-option :vertex-shape
+                   (rc-set-transform setVertexShapeTransformer)
+                   (rc-get getVertexShapeTransformer)
+                   "fn mapping from vertex to Shape")
+   (default-option :vertex-stroke
+                   (rc-set-transform setVertexStrokeTransformer)
+                   (rc-get getVertexStrokeTransformer)
+                   "fn mapping from vertex to Stroke (used for outlines")
+   (default-option :vertex-font
+                   (rc-set-transform setVertexFontTransformer)
+                   (rc-get getVertexFontTransormer)
+                   "fn mapping from vertex to Font (used for vertex labels)")
+   (default-option :edge-label
+                   (rc-set-transform setEdgeLabelTransformer)
+                   (rc-get setEdgeLabelTransformer)
+                   "fn mapping from edge to String (used as a text label)")
+   (default-option :edge-draw
+                   (rc-set-transform setEdgeDrawPaintTransformer)
+                   (rc-get getEdgeDrawPaintTransformer)
+                   "fn mapping from edge to Paint (e.g., edge colors)")
+   (default-option :edge-stroke
+                   (rc-set-transform setEdgeStrokeTransformer)
+                   (rc-get getEdgeStrokeTransformer)
+                   "fn mapping from edge to Stroke (edge weight, dotting, etc.)")
+   (default-option :edge-shape
+                   (rc-set-transform setEdgeShapeTransformer)
+                   (rc-get getEdgeShapeTransformer)
+                   "fn mapping from edge to Shape (for exotic edges)")
+   (default-option :arrow?
+                   (rc-set-predicate setEdgeArrowPredicate)
+                   (rc-get-predicate getEdgeArrowPredicate)
+                   "predicate on [context, edge] to determine arrowhead presence")
+   (default-option :arrow-draw
+                   (rc-set-transform setArrowDrawPaintTransformer)
+                   (rc-get getArrowDrawPaintTransformer)
+                   "fn mapping from [context, edge] to Paint")
+   (default-option :arrow-fill
+                   (rc-set-transform setArrowFillPaintTransformer)
+                   (rc-get getFillDrawPaintTransformer)
+                   "fn mapping from [context, edge] to Paint (inside arrowheads)")
+   (default-option :arrow-stroke
+                   (rc-set-transform setEdgeArrowStrokeTransformer)
+                   (rc-get getFillDrawPaintTransformer)
+                   "fn mapping from [context, edge] to Paint (inside arrowheads)")
+   (default-option :vertex-renderer
+                   vertex-renderer!
+                   (r-get getEdgeRenderer)
+                   "set a JUNG VertexRenderer")
+   (default-option :edge-renderer
+                   (r-set setEdgeRenderer)
+                   (r-get getEdgeRenderer)
+                   "set a JUNG EdgeRenderer")))
 
-(defrecord JungVisualizerHandler [graph layout visualizer])
-
-(defn jung-visualizer-handler [jgh]
-  (let [g (:jg jgh)
-        l (make-layout g 650 650 100 0.75)
-        v (make-visualizer l 650 650)]
-    (JungVisualizerHandler. g l v)))
-
-(defn jung-static-visualizer-handler [jgh vertex-xy-fn]
-  (let [g (:jg jgh)
-        l (make-static-layout g vertex-xy-fn 650 650)
-        v (make-visualizer l 650 650)]
-    (JungVisualizerHandler. g l v)))
+(option-provider VisualizationViewer jung-visualizer-options)
