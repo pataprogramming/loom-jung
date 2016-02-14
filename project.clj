2849168fe583e508969b13a8ca0a1b2299400c4e
Paul L. Snyder <paul@pataprogramming.com>
2016-02-14 12:43:03 -0500

    Typo in project.clj

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://github.io/pataprogramming/loom-jung/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7S.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [seesaw "1.4.5"]
                  [net.sf.jung/jung-api "2.0.1"]
                  [net.sf.jung/jung-graph-impl "2.0.1"]
